3b3401b88caf58c99cb2291307b72e6d5f087010
TheGetGoodGit2211 <jaymideansheldon@gamil.com>
2026-03-26 18:37:09 +0000

    Added conform.nvim to the Neovim config to set up formatting on save

--- nvim/.config/nvim/lua/user/plugins/mason.lua
+++ nvim/.config/nvim/lua/user/plugins/mason.lua
@@ -1,21 +1,32 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
 
-      vim.keymap.set("n", "<leader>ms", ":Mason<CR>")
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig",
-    config = function()
-      local servers = require("user.servers")
+			vim.keymap.set("n", "<leader>ms", ":Mason<CR>")
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig",
+		config = function()
+			local servers = require("user.servers")
 
-      require("mason-lspconfig").setup({
-        ensure_installed = servers
-      })
-    end,
-  },
-  "neovim/nvim-lspconfig",
+			require("mason-lspconfig").setup({
+				ensure_installed = servers,
+			})
+		end,
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer",
+		dependencies = { "williamboman/mason.nvim" },
+		config = function()
+			local formatters = require("user.formatters")
+
+			require("mason-tool-installer").setup({
+				ensure_installed = formatters,
+			})
+		end,
+	},
+	"neovim/nvim-lspconfig",
 }
