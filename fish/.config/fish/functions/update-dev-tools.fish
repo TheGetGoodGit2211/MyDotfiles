6a4bdab8fc328939e57d76cc9f58ed1097da537d
TheGetGoodGit2211 <jaymideansheldon@gamil.com>
2026-07-02 22:14:33 +0100

    corrected some incorrect logic in the update-dev-tools function

--- fish/.config/fish/functions/update-dev-tools.fish
+++ fish/.config/fish/functions/update-dev-tools.fish
@@ -1,28 +1,32 @@
 function update-dev-tools -d "Updates user-space development utilities"
     if command -q rustup
         rustup update
-    else
         echo "[dotfiles] rustup has been updated"
+    else
+        echo "[dotfiles] rustup not found, skipping"
     end
 
     if command -q fnm
         cargo install fnm
         fnm install --lts
-    else
         echo "[dotfiles] fnm has been updated"
+    else
+        echo "[dotfiles] fnm not found, skipping"
     end
 
     if command -q uv
         cargo install uv
-    else
         echo "[dotfiles] uv has been updated"
+    else
+        echo "[dotfiles] uv not found, skipping"
     end
 
     if command -q zoxide
         curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
-    else
         echo "[dotfiles] zoxide has been updated"
+    else
+        echo "[dotfiles] zoxide not found, skipping"
     end
 
-    echo ""
+    echo "[dotfiles] tools updated where possible"
 end
