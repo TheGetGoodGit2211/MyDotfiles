988d9f66e1d4c5d90bf116e3f5dfbdcb41296ddb
TheGetGoodGit2211 <jaymideansheldon@gamil.com>
2026-07-03 00:33:54 +0100

    improve conf.fish to add paths and such conditionally

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,10 +3,14 @@ if test -f $cachy_cfg
     source $cachy_cfg
 end
 
-fish_add_path -U "$HOME/.cargo/bin" "$HOME/.local/bin"
+if command -q cargo
+    fish_add_path -U "$HOME/.cargo/bin" "$HOME/.local/bin"
+end
 
 if command -q fnm
     fnm env --use-on-cd --shell fish | source
 end
 
-zoxide init fish --cmd cd | source
+if command -q zoxide
+    zoxide init fish --cmd cd | source
+end
